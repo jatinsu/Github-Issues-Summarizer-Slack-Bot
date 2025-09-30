d82e1c2a456076fc49a1216ec5a672186031af41
Jatin Suri <jsuri@redhat.com>
2025-09-30 12:13:47 -0400

    Added ability to check for message from bot

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import os
 from slack_bolt import App
 from dotenv import load_dotenv
+# import re
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 load_dotenv()   
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 SLACK_CHANNEL_ID = os.environ.get("SLACK_CHANNEL_ID")
 SLACK_APP_TOKEN = os.environ.get("SLACK_APP_TOKEN")
+
+# Initializes your app with your bot token and socket mode handler
 app = App(token=SLACK_BOT_TOKEN)
 
 
-# This will match any message that contains 👋
-@app.message(":wave:")
-def say_hello(message, say):
-    user = message['user']
-    say(f"Hi there, <@{user}>!")
+@app.message(compile("Issue created by"))
+def handle_message_im(say):
+    say(f"This is a github pull request")
 
+if __name__ == "__main__":
+    SocketModeHandler(app, SLACK_APP_TOKEN).start()
