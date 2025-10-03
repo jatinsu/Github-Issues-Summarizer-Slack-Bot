707f732b063a18f3861dad3b26fd90eff8bdf301
Jatin Suri <jsuri@redhat.com>
2025-10-03 16:18:50 -0400

    added ability to check the message from the github bot

--- app.py
+++ app.py
@@ -10,9 +10,22 @@ SLACK_CHANNEL_ID = os.environ.get("SLACK_CHANNEL_ID")
 SLACK_APP_TOKEN = os.environ.get("SLACK_APP_TOKEN")
 app = App(token=SLACK_BOT_TOKEN)
 
-@app.message("Issue created by")
-def handle_message_im(say):
-    say(f"This is a github pull request")
+@app.message("testing for slack bot")
+def say_hello(say, event):
+    say(f"Hello world")
 
+@app.event("message")
+def handle_message_im(message, say):
+    # check if the message is from github bot
+    if "bot_id" not in message and "bot_id" != "U09GJGSSQ1W":
+        print("the message is not from github bot")
+        return
+    
+    # print the message in the plain text
+    say(f"the user id is {message['user']} and the message is {message['text']}")
+    the_message = message['text']
+    print(the_message)
+
+    
 if __name__ == "__main__":
     SocketModeHandler(app, SLACK_APP_TOKEN).start()
