c267eed6713a59921985dd98937ff9d4863d7af2
Jatin Suri <jsuri@redhat.com>
2025-10-05 13:50:11 -0400

    It can now pull message metadata from the url

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from slack_bolt import App
 from dotenv import load_dotenv
 # import re
+import json
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 load_dotenv()   
@@ -11,20 +12,22 @@ SLACK_APP_TOKEN = os.environ.get("SLACK_APP_TOKEN")
 app = App(token=SLACK_BOT_TOKEN)
 
 @app.message("testing for slack bot")
-def say_hello(say, event):
+def say_hello(say):
     say(f"Hello world")
 
 @app.event("message")
 def handle_message_im(message, say):
     # check if the message is from github bot
-    if "bot_id" not in message and "bot_id" != "U09GJGSSQ1W":
+    if message['bot_id'] != "B09GJGSLK4Y":
         print("the message is not from github bot")
         return
-    
+
     # print the message in the plain text
-    say(f"the user id is {message['user']} and the message is {message['text']}")
-    the_message = message['text']
-    print(the_message)
+    github_issue_link = json.loads(message['attachments'][0]['actions'][0]['value'])['issueHtmlUrl']
+    say(f"The github issue link is {github_issue_link}")
+    issue_message = message['attachments'][0]['text']
+    say(f"The message is {issue_message}")
+    print(f"{json.dumps(message, indent=2)}")
 
     
 if __name__ == "__main__":
